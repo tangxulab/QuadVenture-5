191727f44d10c98ddacbce76e7eb4a1112de388d
tangxulab <m17855827529@163.com>
2021-10-09 13:40:12 +0800

    Revert "Create README.md"
    This reverts commit 3b06bfd8004ba1ad0bcb4baceb2498fe0344ad30.

--- python-space/QuadVenture 5.0.1/protocol/tcp_server.py
+++ python-space/QuadVenture 5.0.1/protocol/tcp_server.py
@@ -40,79 +40,17 @@ class tcp_server(poc.QObject):
         return method()
 
     def tcp_link(self, client_socket, address):
-        #receive_data_all = bytearray()
+        receive_data_all = bytearray()
         # print('asdsa')
         while True:
+            # while True:
             receive_data = client_socket.recv(self.BUFFSIZE)
-            # print('接收', receive_data.hex())
-            # receive_data_all = receive_data_all+receive_data
-            # length = int.from_bytes(receive_data_all[2:4], byteorder='big')
-            # while length >
-            # # if not receive_data:
-            # #     break
-            # # receive_data_all = receive_data_all + receive_data
-            #
-            # # print(receive_data_all.hex())
-            # # length = int.from_bytes(self.receive_data[2:4], byteorder='big')
-            # # # print(length)
-            # command_code = receive_data[1:2]
-            # # # # # # # print(command_code.hex())
-            # command_code = int.from_bytes(command_code, byteorder='big')
-            # if command_code == protocol.DATA_REQUEST_AND_RESPONSE.MS_DATA_FIRST[0]:
-            #     receive_data_all = receive_data_all + receive_data
-            #     receive_data = client_socket.recv(self.BUFFSIZE)
-            #     receive_data_all = receive_data_all + receive_data
-            #     if len(receive_data) < int(self.count / 3) + 7:
-            #         receive_data_all = receive_data_all + receive_data
-            # #
-            # # #     length = int.from_bytes(self.receive_data[2:4], byteorder='big')
-            # # #     if length + 7 == len(self.receive_data):
-            # # #         pass
-            # # #     else:
-            # # #         continue
-            # # #
-            # elif command_code == protocol.DATA_REQUEST_AND_RESPONSE.MS_DATA_THIRD[0]:
-            #     if len(receive_data) < self.count - int(self.count / 3) - int(self.count / 3) + 7:
-            #         receive_data_all = receive_data_all + receive_data
-            # # #     length = int.from_bytes(self.receive_data[2:4], byteorder='big')
-            # # #     print(self.receive_data.hex())
-            # # #     print("第三节", length, len(self.receive_data))
-            # # #     if length + 7 == len(self.receive_data):
-            # # #         print('>>>>>>>>>>>>')
-            # # #
-            # # #     else:
-            # # #         continue
-            # elif command_code == protocol.DATA_REQUEST_AND_RESPONSE.MS_DATA_SECOND[0]:
-            #     if len(receive_data) < int(self.count / 3) + 7:
-            #         receive_data_all = receive_data_all + receive_data
-            # #     length = int.from_bytes(self.receive_data[2:4], byteorder='big')
-            # #     if length + 7 == len(self.receive_data):
-            # #         pass
-            # #     else:
-            # #         continue
-            # else:
-            #     receive_data_all
+            receive_data_all = receive_data_all+receive_data
 
-            # receive_data.clear()
-            # self.y.append(
-            #     int.from_bytes(receive_data[4 + i:5 + i], byteorder='big') for i in range(len(receive_data) - 3))
-            # self.figure = plt.figure()
-            # self.canvas = FigureCanvas(self.figure)
-            # ax = self.figure.add_axes([0.1, 0.1, 0.8, 0.8])
-            # ax.clear()  # 每次绘制一个函数时清空绘图
-            # self.x = np.linspace(0, 5 * np.pi, 400)
-            # self.y = np.sin(self.x)
-            # ax.plot(self.x, self.y)
-            # self.canvas.draw()
-            # print(receive_data.hex())
-
-            # print(receive_data.hex())
-            # print('-------------------------------------------------------------')
-            print(receive_data.hex())
-            # self.just_receive_data(receive_data)
-
-            self.listen_signal.emit(receive_data)
-            # receive_data_all.clear()
+            if receive_data.hex().endswith('aa'):
+                print(receive_data_all.hex())
+                self.listen_signal.emit(receive_data_all)
+                receive_data_all.clear()
 
         # socket.close()
 
